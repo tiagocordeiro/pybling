38f513fd7624ae8ff10fec52bf81a4d45363c9d5
Tiago Cordeiro <tiago@mulhergorila.com>
2018-11-27 16:16:06 -0200

    Itera em todas as páginas - products.list_all

--- pybling/products.py
+++ pybling/products.py
@@ -1,4 +1,5 @@
 import os
+
 import requests
 from dotenv import load_dotenv
 
@@ -7,9 +8,27 @@ load_dotenv()
 BLING_SECRET_KEY = os.getenv("BLING_API_KEY")
 
 
-def list_all():
-    url = 'https://bling.com.br/Api/v2/produtos/json/'
+def list_all(page=1):
     payload = {'apikey': BLING_SECRET_KEY}
+
+    url = f'https://bling.com.br/Api/v2/produtos/page={page}/json/'
+
+    if page == 'all':
+        page = 1
+
+        while True:
+            url = f'https://bling.com.br/Api/v2/produtos/page={page}/json/'
+            produtos = requests.get(url, params=payload)
+            try:
+                pagina = produtos.json()['retorno']['produtos']
+                page += 1
+                for item in pagina:
+                    print(item['produto']['codigo'])
+            except KeyError:
+                print('fim')
+                break
+
+        return produtos
 
     produtos = requests.get(url, params=payload)
     return produtos
@@ -21,3 +40,12 @@ def get_product(codigo):
 
     produto = requests.get(url, params=payload)
     return produto
+
+
+if __name__ == '__main__':
+    # todos_produtos = list_all(page='all')
+    # print(todos_produtos)
+    produto = get_product('PEC-0145')
+    detalhes = produto.json()['retorno']['produtos'][0]['produto']
+    print(detalhes['codigo'])
+    print(detalhes['descricao'])
